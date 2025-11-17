2d067cc0d3e76cdda85b952bbab99eabed660aad
marison77 <marison399@gmail.com>
2025-11-17 19:48:50 +0530

    icons added

--- src/components/Interview_Section/TechSection.jsx
+++ src/components/Interview_Section/TechSection.jsx
@@ -2,40 +2,65 @@ import { useState } from "react";
 import images from "../../assets/Images";
 
 const TechSection = () => {
-const techs = [
-  // ---------- FRONTEND ----------
-  { name: "HTML", bg: "linear-gradient(to right, #E44D26, #F16529)", icon: images.html },
-  { name: "CSS", bg: "linear-gradient(to right, #264de4, #2965f1)", icon: images.css },
-  { name: "Bootstrap", bg: "linear-gradient(to right, #563d7c, #7952b3)", icon: images.bootstrap },
-  { name: "JavaScript", bg: "linear-gradient(to right, #f7df1e, #fbbc04)", icon: images.js },
-  { name: "Angular", bg: "linear-gradient(to right, #dd1b16, #c82312)", icon: images.angular },
-  { name: "Reactjs", bg: "linear-gradient(to right, #00d8ff, #61dafb)", icon: images.react },
 
-  // ---------- BACKEND ----------
-  { name: "C", bg: "linear-gradient(to right, #283593, #3949ab)", icon: images.c },
-  { name: "C++", bg: "linear-gradient(to right, #00599C, #3269ae)", icon: images.cpp },
+  const techs = [
   { name: "Java", bg: "linear-gradient(to right, #f44336, #e91e63)", icon: images.java },
   { name: "Python", bg: "linear-gradient(to right, #ffd449, #4b8bbe)", icon: images.python },
+  { name: "C", bg: "linear-gradient(to right, #283593, #3949ab)", icon: images.c },
+  { name: "C++", bg: "linear-gradient(to right, #00599C, #3269ae)", icon: images.cpp },
+  { name: "JavaScript", bg: "linear-gradient(to right, #f7df1e, #fbbc04)", icon: images.js },
   { name: "Nodejs", bg: "linear-gradient(to right, #3c873a, #68a063)", icon: images.node },
-  { name: "SpringBoot", bg: "linear-gradient(to right, #6db33f, #3a7d2e)", icon: images.spring },
-  { name: "Django", bg: "linear-gradient(to right, #092E20, #0b3b2a)", icon: images.django },
+  { name: "Mysql", bg: "linear-gradient(to right, #00758f, #0096c7)", icon: images.mysql },
 
-  // ---------- DATABASE ----------
-  { name: "MySQL", bg: "linear-gradient(to right, #00758f, #0096c7)", icon: images.mysql },
-  { name: "MongoDB", bg: "linear-gradient(to right, #13aa52, #116149)", icon: images.mongodb },
-
-  // ---------- MOBILE / CROSS-PLATFORM ----------
+  { name: "Mongo db", bg: "linear-gradient(to right, #13aa52, #116149)", icon: images.mongodb },
+  { name: "React js", bg: "linear-gradient(to right, #00d8ff, #61dafb)", icon: images.react },
+  { name: "Angular", bg: "linear-gradient(to right, #dd1b16, #c82312)", icon: images.angular },
   { name: "Flutter", bg: "linear-gradient(to right, #02569B, #039BE5)", icon: images.flutter },
-
-  // ---------- DATA & ANALYTICS ----------
-  { name: "Data Analytics", bg: "linear-gradient(to right, #0f2027, #203a43, #2c5364)", icon: images.analytics },
-
-  // ---------- TESTING ----------
-  { name: "Software Testing", bg: "linear-gradient(to right, #7b4397, #dc2430)", icon: images.testing },
-
-  // ---------- UI / UX ----------
   { name: "UI/UX", bg: "linear-gradient(to right, #ff0099, #493240)", icon: images.ux },
+  { name: "Django", bg: "linear-gradient(to right, #092E20, #0b3b2a)", icon: images.django },
+  { name: "HTML", bg: "linear-gradient(to right, #E44D26, #F16529)", icon: images.html },
+
+  { name: "CSS", bg: "linear-gradient(to right, #264de4, #2965f1)", icon: images.css },
+  { name: "Software Testing", bg: "linear-gradient(to right, #7b4397, #dc2430)", icon: images.testing },
+  { name: "Bootstrap", bg: "linear-gradient(to right, #563d7c, #7952b3)", icon: images.bootstrap },
+  { name: "SpringBoot", bg: "linear-gradient(to right, #6db33f, #3a7d2e)", icon: images.spring },
+  { name: "Data Analytics", bg: "linear-gradient(to right, #0f2027, #203a43, #2c5364)", icon: images.analytics },
 ];
+
+// const techs = [
+//   // ---------- FRONTEND ----------
+//   { name: "HTML", bg: "linear-gradient(to right, #E44D26, #F16529)", icon: images.html },
+//   { name: "CSS", bg: "linear-gradient(to right, #264de4, #2965f1)", icon: images.css },
+//   { name: "Bootstrap", bg: "linear-gradient(to right, #563d7c, #7952b3)", icon: images.bootstrap },
+//   { name: "JavaScript", bg: "linear-gradient(to right, #f7df1e, #fbbc04)", icon: images.js },
+//   { name: "Angular", bg: "linear-gradient(to right, #dd1b16, #c82312)", icon: images.angular },
+//   { name: "Reactjs", bg: "linear-gradient(to right, #00d8ff, #61dafb)", icon: images.react },
+
+//   // ---------- BACKEND ----------
+//   { name: "C", bg: "linear-gradient(to right, #283593, #3949ab)", icon: images.c },
+//   { name: "C++", bg: "linear-gradient(to right, #00599C, #3269ae)", icon: images.cpp },
+//   { name: "Java", bg: "linear-gradient(to right, #f44336, #e91e63)", icon: images.java },
+//   { name: "Python", bg: "linear-gradient(to right, #ffd449, #4b8bbe)", icon: images.python },
+//   { name: "Nodejs", bg: "linear-gradient(to right, #3c873a, #68a063)", icon: images.node },
+//   { name: "SpringBoot", bg: "linear-gradient(to right, #6db33f, #3a7d2e)", icon: images.spring },
+//   { name: "Django", bg: "linear-gradient(to right, #092E20, #0b3b2a)", icon: images.django },
+
+//   // ---------- DATABASE ----------
+//   { name: "MySQL", bg: "linear-gradient(to right, #00758f, #0096c7)", icon: images.mysql },
+//   { name: "MongoDB", bg: "linear-gradient(to right, #13aa52, #116149)", icon: images.mongodb },
+
+//   // ---------- MOBILE / CROSS-PLATFORM ----------
+//   { name: "Flutter", bg: "linear-gradient(to right, #02569B, #039BE5)", icon: images.flutter },
+
+//   // ---------- DATA & ANALYTICS ----------
+//   { name: "Data Analytics", bg: "linear-gradient(to right, #0f2027, #203a43, #2c5364)", icon: images.analytics },
+
+//   // ---------- TESTING ----------
+//   { name: "Software Testing", bg: "linear-gradient(to right, #7b4397, #dc2430)", icon: images.testing },
+
+//   // ---------- UI / UX ----------
+//   { name: "UI/UX", bg: "linear-gradient(to right, #ff0099, #493240)", icon: images.ux },
+// ];
 
 
   const [searchTerm, setSearchTerm] = useState("");
